689f26456bab1fb60ee8d018338ff92c541e2b18
amcnulty <amcnulty88@swbell.net>
2016-01-28 15:36:52 -0600

    Footer, Scrolling, Map Links
    Added a footer to the page in the same color scheme.
    
    Made the <nav> buttons work and change color when hovered over.
    
    Made the links in the map go to their respective pages.

--- script/app.js
+++ script/app.js
@@ -12,8 +12,30 @@ window.onload = function() {
         map.className = "";
     }
     
+    function scrollToTop() {
+        $('html, body').animate({scrollTop: 0}, 600);
+    }
+
+    function scrollToAbout() {
+        $('html, body').animate({scrollTop: $("#info").offset().top - 160}, 600);
+    }
+
+    function scrollToLocations() {
+        $('html, body').animate({scrollTop: $("#mapSection").offset().top - 160}, 600);
+    }
+
     function mouseOut(e) {
-        if (e.clientY + document.body.scrollTop <= 1728) {
+        // alert(e.clientY + document.body.scrollTop);
+        if (e.clientY + document.body.scrollTop <= 1707) {
+            map.className = "scrollOff";
+        }
+        else if (e.clientY + document.body.scrollTop >= 2406) {
+            map.className = "scrollOff";
+        }
+        else if (e.clientX <= window.innerWidth * .1) {
+            map.className = "scrollOff";
+        }
+        else if (e.clientX >= window.innerWidth * .9 - 15) {
             map.className = "scrollOff";
         }
     }
@@ -25,4 +47,8 @@ window.onload = function() {
     mapCanvas.addEventListener("click", mapClicked, false);
     map.addEventListener("mouseout", mouseOut, false);
     document.addEventListener("scroll", setScroll, false);
+    document.getElementById("homeNavButton").addEventListener("click", scrollToTop, false);
+    document.getElementById("aboutNavButton").addEventListener("click", scrollToAbout, false);
+    document.getElementById("locationsNavButton").addEventListener("click", scrollToLocations, false);
+    document.getElementById("headerLogoImg").addEventListener("click", scrollToTop, false);
 }
