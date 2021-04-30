12b8d749269190b65ef8c2476bacfcf0a0d5dc92
amcnulty <amcnulty88@swbell.net>
2021-04-29 21:37:21 -0500

    Ping Endpoint
    Added a ping endpoint for keeping the app awake.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const startDate = new Date();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+/**
+ * Endpoint used to ping the server to keep the app awake.
+ */
+ router.get('/ping', (req, res) => {
+  res.status(200).send(`The app has been awake since: ${startDate}`);
 });
 
 module.exports = router;
