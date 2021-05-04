6fc34c8cd408737dbcca03d192b4f2a9884a8090
amcnulty <amcnulty88@swbell.net>
2021-05-03 20:31:54 -0500

    Map Work
    Added a map to the directory page using leaflet and react-leaflet.

--- client/src/routes/directory/Directory.js
+++ client/src/routes/directory/Directory.js
@@ -1,7 +1,44 @@
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+
+function EventComponent(props) {
+    const map = useMapEvents({
+        zoom: (event) => {
+            props.setZoom(map.getZoom());
+        }
+    })
+    
+    return null;
+}
+
 function Directory(props) {
+    const [zoom, setZoom] = useState(5);
     return (
         <div>
             Directory Works!
+            <MapContainer
+                center={[39.087692, -97.611850]}
+                zoom={5}
+                scrollWheelZoom={true}
+                style={{height: '800px'}}
+            >
+                <EventComponent setZoom={setZoom}/>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    opacity={zoom <= 7 ? 1 : 0}
+                />
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                    opacity={zoom > 7 ? 1 : 0}
+                />
+                <Marker position={[39.087692, -94.611850]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     );
 }
