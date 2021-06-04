e249eef973b9685f2323b37d78e239627a2c176c
amcnulty <amcnulty88@swbell.net>
2021-06-03 20:12:03 -0500

    Locations Page Work
    Added some more content to the locations page.
    Fixed a bug in the header where text was not visible on mobile.
    Added a locations data file to help reference when creating locations.

--- client/src/routes/directory/Directory.js
+++ client/src/routes/directory/Directory.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import { API } from '../../util/API';
 import './directory.sass';
@@ -17,6 +17,8 @@ function EventComponent(props) {
 function Directory(props) {
     const [zoom, setZoom] = useState(5);
     const [locations, setLocations] = useState([]);
+    const history = useHistory();
+    const MAP_ICONS = ['fa-map-signs', 'fa-map-pin', 'fa-map-marker-alt', 'fa-directions', 'fa-road', 'fa-location-arrow', 'fa-map-marked-alt', 'fa-globe-americas'];
 
     useEffect(() => {
         console.log('getting all locations');
@@ -27,6 +29,12 @@ function Directory(props) {
 
     return (
         <div className='Directory'>
+            <div className="landingSection pt-5">
+                <div className="container">
+                    <h1 className='fw-light text-uppercase'>LOCATIONS</h1>
+                    <p>View our many locations. Interact with the map below and click on map markers to see more information about a specific location.</p>
+                </div>
+            </div>
             <div className="container py-5">
                 <MapContainer
                     center={[39.087692, -97.611850]}
@@ -80,6 +88,23 @@ function Directory(props) {
                         ))
                     }
                 </MapContainer>
+            </div>
+            <div className='listingSection themeBackground py-5 position-relative'>
+                <div className="container">
+                    <div className="row">
+                        <div className="iconContainer position-absolute">
+                            {MAP_ICONS.map((name, index) => <i key={index} className={`fas ${name}`}></i>)}
+                            {MAP_ICONS.map((name, index) => <i key={index + 29} className={`fas ${name}`}></i>)}
+                            {MAP_ICONS.map((name, index) => <i key={index + 49} className={`fas ${name}`}></i>)}
+                            {MAP_ICONS.map((name, index) => <i key={index + 69} className={`fas ${name}`}></i>)}
+                        </div>
+                        <div className="col text-center checkUsOutContainer">
+                            <h2>Get On The Map</h2>
+                            <p className='fst-italic'>List with us to get your location displayed on the map.</p>
+                            <button type='button' className="btn btn-primary" onClick={() => history.push('/development-services')}>List With Us</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
