3076d0621a02fba7cd5503f6bab874681e199114
amcnulty <amcnulty88@swbell.net>
2021-06-12 09:21:24 -0500

    About Page Changes
    Removed the orange background sections with the icons and added some plain text with the contractors garage story.

--- client/src/routes/about-us/AboutUs.js
+++ client/src/routes/about-us/AboutUs.js
@@ -1,7 +1,5 @@
 import CompanyLogo from '../../components/companyLogo/CompanyLogo';
-import HighlightBox from '../../components/highlightBox/HighlightBox';
 import './aboutUs.sass';
-import locationsImage from './images/locationsImage.png';
 import pinsImage from './images/pins.png';
 import { useHistory } from 'react-router';
 
@@ -16,28 +14,17 @@ function AboutUs(props) {
                     <CompanyLogo textOnly/>
                 </div>
             </div>
-            <div className="highlightSection themeBackground py-5">
+            <div className="storySection py-5">
                 <div className="container">
-                    <div className="row">
-                        <div className="col col-md-6 col-lg-4 d-flex justify-content-center">
-                            <HighlightBox
-                                thumbnail={locationsImage}
-                                label='Multiple Locations'
-                            />
-                        </div>
-                        <div className="col col-md-6 col-lg-4 d-flex justify-content-center">
-                            <HighlightBox
-                                iconName='fas fa-temperature-low'
-                                label='Insulated Units'
-                            />
-                        </div>
-                        <div className="col col-md-6 col-lg-4 d-flex justify-content-center">
-                            <HighlightBox
-                                iconName='fas fa-plug'
-                                label='Electric service'
-                            />
-                        </div>
-                    </div>
+                    <p>
+                        Contractors Garage was created after the advice from a cousin in development and inspiration from Robert Kiyosaki’s book, Rich Dad Poor Dad, to leave the corporate rat race, along with its comforts, to jump into a 100% sales commission career in commercial-industrial real estate brokerage.  Eager to make a few bucks during those initial months, Kevin was happy to accept the meager income from those least desirable leads from the tiniest of entrepreneurs trying to make that jump from their garage or mini storage to a place of their own.  They needed about 500 square feet for about $500 per month.  Problem is… those spaces did not exist in Kansas City.  Filling the trash can full of good leads gone cold, we decided to build our first Contractors Garage in 2009 in the middle of a recession using private funds.  We quickly learned our spaces were recession proof, filling up before construction finished and in a poor economy with not only tenants moving up, but also tenants downsizing in a time when office/flex space was no longer required for some.
+                    </p>
+                    <p>
+                        Though many have since imitated our concept in Kansas City, Contractors Garage is the genuine original brand for small incubator spaces in KC specifically tailored for business owners.  Today, rather than fighting our competition, we chose to help by sharing the brand with others through our new listing service at <a href='/development-services' target='_blank'>ContractorsGarage.com</a>.  If you have a similar property that would benefit from a national brand, your own personalized web page, optional credit card payments, among other features and benefits, we are here to help.
+                    </p>
+                    <p>
+                        Would you also like to leave the rat race and build passive income through commercial real estate investment?  Please see us at <a href='http://www.insightcommercial.net/construction.html'>www.insightcommercial.net/construction</a> for personalized one-on-one consulting.
+                    </p>
                 </div>
             </div>
             <div className="locationImageSection"></div>
@@ -47,30 +34,6 @@ function AboutUs(props) {
                     <p className='fw-bold'>
                         Contractors Garage offers large functional industrial space designed exclusively for entrepreneurs and hobbyists including but not limited to landscapers, plumbers, electricians, specialty vehicles, or about anyone needing space larger than mini storage but smaller and less expensive than traditional incubator industrial space.
                     </p>
-                </div>
-            </div>
-            <div className="highlightSection themeBackground py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col col-md-6 col-lg-4 d-flex justify-content-center">
-                            <HighlightBox
-                                iconName='fas fa-arrows-alt-h'
-                                label='Large drive-in doors'
-                            />
-                        </div>
-                        <div className="col col-md-6 col-lg-4 d-flex justify-content-center">
-                            <HighlightBox
-                                iconName='fas fa-parking'
-                                label='Huge parking lots'
-                            />
-                        </div>
-                        <div className="col col-md-6 col-lg-4 d-flex justify-content-center">
-                            <HighlightBox
-                                iconName='fas fa-business-time'
-                                label='Tailored For Business'
-                            />
-                        </div>
-                    </div>
                 </div>
             </div>
             <div className="parallaxSection"></div>
