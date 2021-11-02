937bbefab285cecf97214dca403410ecb6c7c436
amcnulty <amcnulty88@swbell.net>
2021-11-02 17:57:06 -0500

    Map Fix
    Added a fix for Safari browsers where popups on map were not opening.
    Moved the list of locations above the map.

--- client/src/routes/directory/Directory.js
+++ client/src/routes/directory/Directory.js
@@ -40,74 +40,10 @@ function Directory(props) {
 
     return (
         <div className='Directory'>
-            <div className="landingSection pt-3">
+            <div className="landingSection py-3">
                 { locations && locations.filter(location => location.units && location.units.some(unit => unit.available)).length > 0 && <Alert className='text-center unitAlert' color='success'>We have locations with <b>units available</b>. Check the map and list below to find a location with units ready to rent.</Alert>}
                 <div className="container">
                     <h1 className='fw-light text-uppercase'>LOCATIONS</h1>
-                    <p>View our many locations. Interact with the map below and click on map markers to see more information about a specific location.</p>
-                </div>
-            </div>
-            <div className="container">
-                <MapContainer
-                    center={center}
-                    zoom={zoom}
-                    scrollWheelZoom={true}
-                >
-                    <EventComponent/>
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        opacity={zoom <= 7 ? 1 : 0}
-                    />
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-                        opacity={zoom > 7 ? 1 : 0}
-                    />
-                    <MarkerClusterGroup>
-                    {
-                        locations.map(location => (
-                            <Marker
-                                key={location._id}
-                                position={location.coordinates}
-                                riseOnHover={true}
-                            >
-                                <Popup keepInView={true}>
-                                    <h6
-                                        className='text-nowrap text-truncate'
-                                        title={location.name}
-                                    >{location.name}</h6>
-                                    { location.units && location.units.filter(unit => unit.available).length > 0 && <span className='badge bg-success w-50'>Units Available</span>}
-                                    <address className='mb-0'>
-                                        <p
-                                            className='text-nowrap text-truncate m-0'
-                                            title={location.addressFirstLine}
-                                            >
-                                            {location.addressFirstLine}
-                                        </p>
-                                        <p
-                                            className='text-nowrap text-truncate m-0'
-                                            title={location.addressFirstLine}
-                                        >
-                                            {location.addressSecondLine}
-                                        </p>
-                                    </address>
-                                    <p>
-                                        {location.shortDescription}
-                                    </p>
-                                    {location.thumbnailImage && <img src={location.thumbnailImage.src} alt={location.thumbnailImage.alt}/>}
-                                    <Link className='btn btn-outline-primary mt-3 text-nowrap' to={`/location/${location.slug}`}>View Location <i className="fas fa-arrow-right"></i></Link>
-                                        
-                                </Popup>
-                            </Marker>
-                        ))
-                    }
-                    </MarkerClusterGroup>
-                </MapContainer>
-            </div>
-            <div className="locationListSection py-5">
-                <div className="container">
-                    <h3 className='fw-normal text-uppercase'>Our Locations</h3>
                     <p>See a list of all our locations below. Locations marked with <span className='badge bg-success'>Units Available</span> currently have vacant units ready to rent.</p>
                     <div className="row">
                     {
@@ -125,6 +61,68 @@ function Directory(props) {
                         ))
                     }
                     </div>
+                </div>
+            </div>
+            <div className='mapSection py-5'>
+                <div className="container">
+                    <p>Interact with the map below and click on map markers to see more information about a specific location.</p>
+                    <MapContainer
+                        center={center}
+                        zoom={zoom}
+                        scrollWheelZoom={true}
+                        tap={false}
+                    >
+                        <EventComponent/>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            opacity={zoom <= 7 ? 1 : 0}
+                        />
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                            opacity={zoom > 7 ? 1 : 0}
+                        />
+                        <MarkerClusterGroup>
+                        {
+                            locations.map(location => (
+                                <Marker
+                                    key={location._id}
+                                    position={location.coordinates}
+                                    riseOnHover={true}
+                                >
+                                    <Popup keepInView={true}>
+                                        <h6
+                                            className='text-nowrap text-truncate'
+                                            title={location.name}
+                                        >{location.name}</h6>
+                                        { location.units && location.units.filter(unit => unit.available).length > 0 && <span className='badge bg-success w-50'>Units Available</span>}
+                                        <address className='mb-0'>
+                                            <p
+                                                className='text-nowrap text-truncate m-0'
+                                                title={location.addressFirstLine}
+                                                >
+                                                {location.addressFirstLine}
+                                            </p>
+                                            <p
+                                                className='text-nowrap text-truncate m-0'
+                                                title={location.addressFirstLine}
+                                            >
+                                                {location.addressSecondLine}
+                                            </p>
+                                        </address>
+                                        <p>
+                                            {location.shortDescription}
+                                        </p>
+                                        {location.thumbnailImage && <img src={location.thumbnailImage.src} alt={location.thumbnailImage.alt}/>}
+                                        <Link className='btn btn-outline-primary mt-3 text-nowrap' to={`/location/${location.slug}`}>View Location <i className="fas fa-arrow-right"></i></Link>
+                                            
+                                    </Popup>
+                                </Marker>
+                            ))
+                        }
+                        </MarkerClusterGroup>
+                    </MapContainer>
                 </div>
             </div>
             <div className='listingSection themeBackground py-5 position-relative'>
